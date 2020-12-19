54fe9d9385d309ebedb3afa46b0c9e99b45d02a1
Alonso Marco-Valle <marcovalle.a@gmail.com>
2020-12-19 03:56:53 +0100

    Minor

--- classireg/experiments/numerical_benchmarks/loop_BOC.py
+++ classireg/experiments/numerical_benchmarks/loop_BOC.py
@@ -20,6 +20,7 @@ np.set_printoptions(linewidth=10000)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float32
 
+
 def run(cfg: DictConfig, rep_nr: int) -> None:
 
     # Random seed for numpy and torch:
